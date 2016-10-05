18cd904d79a762b3a2634823e0b6d11e725ce196
AntiDroid <talip.vu@gmail.com>
2016-10-05 14:53:36 +0200

    Add: basic GUI

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -31,11 +32,37 @@ public class Main extends Application {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Main.class.getResource("GUI.fxml"));
             rootLayout = (BorderPane) loader.load();
+            
+            rootLayout.getLeft().setStyle("-fx-padding: 10;" + 
+                    "-fx-border-style: solid inside;" + 
+                    "-fx-border-width: 2;" +
+                    "-fx-border-insets: 5;" + 
+                    "-fx-border-radius: 5;" + 
+                    "-fx-border-color: black;");
+            
+            rootLayout.getRight().setStyle("-fx-padding: 10;" + 
+                    "-fx-border-style: solid inside;" + 
+                    "-fx-border-width: 2;" +
+                    "-fx-border-insets: 5;" + 
+                    "-fx-border-radius: 5;" + 
+                    "-fx-border-color: gray;");
+            
+            rootLayout.getCenter().setStyle("-fx-padding: 10;" + 
+                    "-fx-border-style: solid inside;" + 
+                    "-fx-border-width: 2;" +
+                    "-fx-border-insets: 5;" + 
+                    "-fx-border-radius: 5;" + 
+                    "-fx-border-color: red;");
+            
+            // load the image
+            Image image = new Image("file:res/img/p.png");
+            
+            // simple displays ImageView the image as is
+            ((ImageView)rootLayout.getCenter()).setImage(image);
 
             // Show the scene containing the root layout.
             Scene scene = new Scene(rootLayout);
             
-            primaryStage.getIcons().add(new Image("file:icon.png"));
             primaryStage.setScene(scene);
             primaryStage.setResizable(false);
             primaryStage.show();
