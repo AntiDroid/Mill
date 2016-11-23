c0fceb8382e1b976ce0df7ea12eca643002b6d9c
AntiDroid <AntiDroid@github.com>
2016-11-23 18:27:10 +0100

    Fix: stones couldn't be set

--- src/application/JFXController.java
+++ src/application/JFXController.java
@@ -10,11 +10,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 
 public class JFXController implements Initializable {
-	
+
 	@FXML
 	private Pane mainPane;
 	@FXML
@@ -24,77 +25,92 @@ public class JFXController implements Initializable {
 	@FXML
 	private ImageView feld;
 	@FXML
-	private ImageView test;
-	
+	private GridPane greenGridSel;
+	@FXML
+	private GridPane redGridSel;
+
 	private boolean isRedTurn = true;
-	
-	private int counter = 9;
-	
+
+	private int counter = 0;
+
 	Position[] pos = { new Position(0, 0, 0, 44, 70.5), new Position(0, 0, 1, 44, 249), new Position(0, 0, 2, 44, 428), new Position(0, 1, 0, 222.5, 70.5),
 			new Position(0, 2, 0, 401, 70.5), new Position(0, 2, 1, 401, 249), new Position(0, 2, 2, 401, 428), new Position(0, 1, 2, 222.5, 428),
 			new Position(1, 0, 0, 98, 125), new Position(1, 1, 0, 222.5, 125), new Position(1, 2, 0, 346, 125), new Position(1, 0, 1, 98, 249), new Position(1, 0, 2, 98, 372),
-			new Position(1, 1, 2, 222.5, 372), new Position(1, 2, 2, 346, 372), new Position(1, 2, 1, 346, 249), 
-			new Position(2, 0, 0, 153, 179.5), new Position(2, 0, 1, 153, 249), new Position(2, 0, 2, 153, 316), new Position(2, 1, 0, 222.5, 179.5), 
+			new Position(1, 1, 2, 222.5, 372), new Position(1, 2, 2, 346, 372), new Position(1, 2, 1, 346, 249),
+			new Position(2, 0, 0, 153, 179.5), new Position(2, 0, 1, 153, 249), new Position(2, 0, 2, 153, 316), new Position(2, 1, 0, 222.5, 179.5),
 			new Position(2, 2, 0, 291.5, 179.5), new Position(2, 2, 1, 291.5, 249), new Position(2, 2, 2, 291.5, 316), new Position(2, 1, 2, 222.5, 316)};
 
     @Override // This method is called by the FXMLLoader when initialization is complete
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-    	
+
         assert greenSel != null : "fx:id=\"greenSel\" was not injected: check your FXML file 'GUI.fxml'.";
         assert redSel != null : "fx:id=\"redSel\" was not injected: check your FXML file 'GUI.fxml'.";
-        
+
         assert mainPane != null : "fx:id=\"mainPane\" was not injected: check your FXML file 'GUI.fxml'.";
 
         // initialize your logic here: all @FXML variables will have been injected
-        
+
         Image imageG = new Image("file:res/img/grun.png");
         Image imageR = new Image("file:res/img/rot.png");
         Image imageF = new Image("file:res/img/mill.png");
-        
+
         feld.setImage(imageF);
-        
+
         for(int i = 0; i<9; i++){
         	greenSel.get(i).setImage(imageG);
         	redSel.get(i).setImage(imageR);
         }
-        
+
         mainPane.setOnMouseClicked(new EventHandler<MouseEvent>(){
-        	
+
             @Override
             public void handle(MouseEvent t) {
-            	
-            	if(counter > 0){
-            	
+
+            	if(counter < 9){
             		for(int i = 0; i < pos.length; i++){
+
             			if(pos[i].isInRange(t.getX(), t.getY())){
-            				test.setLayoutX(pos[i].getKoordX());
-            				test.setLayoutY(pos[i].getKoordY());
+
+            				if(!pos[i].isBelegt()){
+                				pos[i].setBelegt(true);
+
+            					if(isRedTurn){
+            						redGridSel.getChildren().remove(redSel.get(counter));
+            						mainPane.getChildren().add(redSel.get(counter));
+            						redSel.get(counter).setLayoutX(pos[i].getKoordX());
+            						redSel.get(counter).setLayoutY(pos[i].getKoordY());
+
+            						break;
+            					}
+            					else{
+            						greenGridSel.getChildren().remove(greenSel.get(counter));
+            						mainPane.getChildren().add(greenSel.get(counter));
+            						greenSel.get(counter).setLayoutX(pos[i].getKoordX());
+            						greenSel.get(counter).setLayoutY(pos[i].getKoordY());
+
+            						counter++;
+            						break;
+            					}
+            				}
             			}
             		}
-            
-            		
-            		if(!isRedTurn)
-            		counter--;
-            	
+
             		isRedTurn = !isRedTurn;
-            	
             	}
+
             }
-            
+
         });
-        
-        test.setImage(imageR);   
-        
-        
+
         //Testsequenz
         /*
         (new Thread() {
 			  public void run() {
                 for (int i = 0; i < pos.length; i++){
-                    
+
                 	test.setLayoutX(pos[i].getKoordX());
                 	test.setLayoutY(pos[i].getKoordY());
-                	
+
                 	try {
             			Thread.sleep(1000);
             		} catch (InterruptedException e) {
@@ -104,10 +120,6 @@ public class JFXController implements Initializable {
 			  }
         }).start();
         */
-    }
-    
-    public ImageView getTest(){
-    	return test;
     }
 
 }
--- src/data/Position.java
+++ src/data/Position.java
@@ -4,13 +4,15 @@ public class Position {
 
 	private int ebene, x, y;
 	private double koordX, koordY;
-	
+	private boolean isBelegt;
+
 	public Position(int ebene, int x, int y, double kX, double kY){
 		this.ebene = ebene;
 		this.x = x;
 		this.y = y;
 		this.koordX = kX;
 		this.koordY = kY;
+		this.isBelegt = false;
 	}
 
 	public int getEbene() {
@@ -23,8 +25,8 @@ public class Position {
 
 	public int getX() {
 		return x;
-	}	
-	
+	}
+
 	public String toString(){
 		return "("+ebene+", "+x+", "+y+")";
 	}
@@ -36,14 +38,24 @@ public class Position {
 	public double getKoordX() {
 		return koordX;
 	}
-	
+
 	public boolean isInRange(double posX, double posY){
-		
-		int rangeDif = 5;
-		
+
+		int rangeDif = 15;
+
 		if(Math.abs(koordX+20 - posX) < rangeDif && Math.abs(koordY+20 - posY) < rangeDif)
 			return true;
-		
+
 		return false;
 	}
+
+	public boolean isBelegt() {
+		return isBelegt;
+	}
+
+	public void setBelegt(boolean isBelegt) {
+		this.isBelegt = isBelegt;
+	}
+
+
 }
