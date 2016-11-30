d6900895e2b3340c11db310958e03304fc4c34eb
Michael Krapf <Michael Krapf@Michael-Laptop>
2016-11-30 08:45:43 +0100

    stein

--- src/data/Stein.java
+++ src/data/Stein.java
@@ -3,22 +3,62 @@ package data;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class Stein {
-
+public class Stein extends ImageView
+{
     static Image imageG = new Image("file:res/img/grun.png");
     static Image imageR = new Image("file:res/img/rot.png");
 	
 	private boolean isRed, isThree;
-	private ImageView img;
+	private Image img;
 	
 	public Stein(boolean isR){
 		if(isR)
-			img.setImage(imageR);
+			img = imageR;
 		else
-			img.setImage(imageG);
+			img = imageG;
 		
 		this.isRed = isR;
 		this.isThree = false;
+	}
+
+	public static Image getImageG() {
+		return imageG;
+	}
+
+	public static void setImageG(Image imageG) {
+		Stein.imageG = imageG;
+	}
+
+	public static Image getImageR() {
+		return imageR;
+	}
+
+	public static void setImageR(Image imageR) {
+		Stein.imageR = imageR;
+	}
+
+	public boolean isRed() {
+		return isRed;
+	}
+
+	public void setRed(boolean isRed) {
+		this.isRed = isRed;
+	}
+
+	public boolean isThree() {
+		return isThree;
+	}
+
+	public void setThree(boolean isThree) {
+		this.isThree = isThree;
+	}
+
+	public Image getImg() {
+		return img;
+	}
+
+	public void setImg(Image img) {
+		this.img = img;
 	}
 	
 }
