e0af0759da6c395c830cf7ff57b43cc6e71a1895
Michael Krapf <Michael Krapf@Michael-Laptop>
2016-12-14 09:59:45 +0100

    jo

--- src/data/DBManager.java
+++ src/data/DBManager.java
@@ -2,6 +2,7 @@ package data;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DBManager 
@@ -13,19 +14,34 @@ public class DBManager
 		conn = DriverManager.getConnection("jdbc:mysql://localhost/rankingsystem", "root", "");
 	}
 	
-	public void addBenutzer(int benutzerID, String vorname, String nachname, int punktezahl) throws SQLException {
-		
-		String sql = "INSERT INTO Benutzer VALUES(?, ?, ?, ?)";
-		java.sql.PreparedStatement stmt = conn.prepareStatement(sql);
+	public void addBenutzer(int benutzerID, String benutzername, int punktezahl) throws SQLException 
+	{
+		String sql = "INSERT INTO Benutzer VALUES(?, ?, ?)";
+		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setInt(1, benutzerID);
-		stmt.setString(2, vorname);
-		stmt.setString(3, nachname);
-		stmt.setInt(4, punktezahl);
-		stmt.executeUpdate(sql);
+		stmt.setString(2, benutzername);
+		stmt.setInt(3, punktezahl);
+		stmt.executeUpdate();
+		stmt.close();
+	}
+	
+	public void changePoints(int benutzerID, int punktezahl) throws SQLException
+	{
+		String sql = "UPDATE Benutzer SET Punktezahl = ? WHERE BenutzerID = ?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, punktezahl);
+		stmt.setInt(2, benutzerID);
+		stmt.executeUpdate();
 		stmt.close();
 	}
 	
 	public void close() throws SQLException {
 		conn.close();
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		DBManager db = new DBManager();
+		db.addBenutzer(3, "Tali", 500);
+		db.changePoints(3, 800);
 	}
 }
