3f8126637d0cda8638e6a6bdc924da2faa5f3d80
AntiDroid <AntiDroid@github.com>
2017-01-17 19:51:02 +0100

    Add: name dialog

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,8 @@ package application;
 
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 import data.Stein;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -13,10 +15,46 @@ public class Main extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
-    
+
+    static public String name1, name2;
+
     @Override
     public void start(Stage primaryStage) {
-    	
+
+		try{
+
+		do{
+		name1 = JOptionPane.showInputDialog(null, "Enter your name!",
+                "BLACK",
+                JOptionPane.PLAIN_MESSAGE);
+
+		if(name1.equals(""))
+			throw new NullPointerException();
+
+		}while(!name1.matches("\\w+"));
+
+		}
+		catch(NullPointerException n){
+			name1 = "Player 1";
+		}
+
+		try{
+
+		do{
+		name2 = JOptionPane.showInputDialog(null, "Enter your name!",
+                "WHITE",
+                JOptionPane.PLAIN_MESSAGE);
+
+		if(name2.equals(""))
+			throw new NullPointerException();
+
+		}while(name2.equals("") || !name2.matches("\\w+") || name2.equals(name1));
+
+		}
+		catch(NullPointerException n){
+			name2 = "Player 2";
+		}
+
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Mill");
 
@@ -32,38 +70,38 @@ public class Main extends Application {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Main.class.getResource("GUI.fxml"));
             rootLayout = (BorderPane) loader.load();
-            
+
             //JFXController f = (JFXController) loader.getController();
 
-            s.getIcons().add(Stein.getImageG()); 
-            
-            rootLayout.getLeft().setStyle("-fx-padding: 10;" + 
-                    "-fx-border-style: solid inside;" + 
+            s.getIcons().add(Stein.getImageG());
+
+            rootLayout.getLeft().setStyle("-fx-padding: 10;" +
+                    "-fx-border-style: solid inside;" +
                     "-fx-border-width: 5;" +
-                    "-fx-border-insets: 1;" + 
-                    "-fx-border-radius: 15;" + 
+                    "-fx-border-insets: 1;" +
+                    "-fx-border-radius: 15;" +
                     "-fx-border-color: DarkTurquoise;");
-            
-            rootLayout.getRight().setStyle("-fx-padding: 10;" + 
-                    "-fx-border-style: solid inside;" + 
+
+            rootLayout.getRight().setStyle("-fx-padding: 10;" +
+                    "-fx-border-style: solid inside;" +
                     "-fx-border-width: 5;" +
-                    "-fx-border-insets: 1;" + 
-                    "-fx-border-radius: 15;" + 
+                    "-fx-border-insets: 1;" +
+                    "-fx-border-radius: 15;" +
                     "-fx-border-color: CornflowerBlue;");
 
             // Show the scene containing the root layout.
             Scene scene = new Scene(rootLayout);
-            
+
             primaryStage.setScene(scene);
             primaryStage.setResizable(false);
             primaryStage.show();
-            
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        launch(args); 
+        launch(args);
     }
 }
