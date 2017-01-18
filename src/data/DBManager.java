85bf135bab4fa01202888d29c5d28f4db5fd01e3
Michael Krapf <Michael Krapf@10.10.209.205>
2017-01-18 08:30:08 +0100

    jo

--- src/data/DBManager.java
+++ src/data/DBManager.java
@@ -59,4 +59,15 @@ public class DBManager
 	{
 		conn.close();
 	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException
+	{
+		DBManager db = new DBManager();
+		db.addBenutzer("Michl", 5000);
+		for(Benutzer b: db.viewBenutzer()){
+			System.out.print(b.getBenutzername()+" ");
+			System.out.println(b.getPunktezahl());
+		}
+		
+	}
 }
